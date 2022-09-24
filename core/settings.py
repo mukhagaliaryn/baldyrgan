04a4fa4f8c0303ab120efc43dceb83c2f151e108
mukhagaliaryn <mukhagaliaryn@gmail.com>
2022-09-24 22:49:55 +0600

    DOne

--- core/settings.py
+++ core/settings.py
@@ -1,20 +1,14 @@
 import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-+!lx)m*%_5l2ghzt#@w-17&i^6=$eys8ex%#dftif@sc%u0z=u'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['baldyrgan.pythonanywhere.com', '127.0.0.1', ]
 
 
 # Application definition
