005388392e12775f4161fdac4a1008bba54aeabc
mukhagaliaryn <mukhagaliaryn@gmail.com>
2022-09-29 11:22:59 +0600

    Done

--- core/settings.py
+++ core/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
 
-DEBUG = config('DEBUG')
+DEBUG = config("DEBUG") == "True"
 
 ALLOWED_HOSTS = ['baldyrgan.pythonanywhere.com', '127.0.0.1', ]
 
